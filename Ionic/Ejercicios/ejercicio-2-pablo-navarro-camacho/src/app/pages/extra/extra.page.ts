af69165ab810c14f5b19be7d26d9083b59e8a287
Pablo <pnavarroc@iesch.org>
2026-09-23 16:55:45 +0200

    Ejercicio realizado completo

--- Ionic/Ejercicios/ejercicio-2-pablo-navarro-camacho/src/app/pages/extra/extra.page.ts
+++ Ionic/Ejercicios/ejercicio-2-pablo-navarro-camacho/src/app/pages/extra/extra.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import {
+  ActionSheetButton,
+  IonActionSheet,
   IonAvatar,
   IonButton,
   IonChip,
@@ -20,9 +22,51 @@ import {GridHomeComponent} from "../../common/grid-home/grid-home.component";
   selector: 'app-extra',
   templateUrl: './extra.page.html',
   styleUrls: ['./extra.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCol, IonButton, RouterLink, IonChip, IonAvatar, IonLabel, GridHomeComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonGrid, IonRow, IonCol, IonButton, RouterLink, IonChip, IonAvatar, IonLabel, GridHomeComponent, IonActionSheet]
 })
 export class ExtraPage implements OnInit {
+
+  imageAvatar1 = signal('/assets/images/anime01.jpg');
+  imageAvatar2 = signal('/assets/images/anime02.jpg');
+  imageAvatar3 = signal('/assets/images/anime03.jpg');
+  protected SelectFauvorite: ActionSheetButton[] = [
+    {
+      text: 'Default',
+      handler: () => {
+        this.imageAvatar1.set('/assets/images/anime01.jpg');
+        this.imageAvatar2.set('/assets/images/anime02.jpg');
+        this.imageAvatar3.set('/assets/images/anime03.jpg');
+      }
+    },
+    {
+      text: 'Mary',
+      handler: () => {
+        this.imageAvatar1.set('/assets/images/anime01.jpg');
+        this.imageAvatar2.set('/assets/images/anime01.jpg');
+        this.imageAvatar3.set('/assets/images/anime01.jpg');
+      }
+    },
+    {
+      text: 'Lisa',
+      handler: () => {
+        this.imageAvatar1.set('/assets/images/anime02.jpg');
+        this.imageAvatar2.set('/assets/images/anime02.jpg');
+        this.imageAvatar3.set('/assets/images/anime02.jpg');
+      }
+    },
+    {
+      text: 'John',
+      handler: () => {
+        this.imageAvatar1.set('/assets/images/anime03.jpg');
+        this.imageAvatar2.set('/assets/images/anime03.jpg');
+        this.imageAvatar3.set('/assets/images/anime03.jpg');
+      }
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel'
+    }
+  ];
 
   constructor() { }
 
--- Ionic/Ejercicios/ejercicio-2-pablo-navarro-camacho/src/app/pages/more/more.page.ts
+++ Ionic/Ejercicios/ejercicio-2-pablo-navarro-camacho/src/app/pages/more/more.page.ts
@@ -1,20 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonChip,
+  IonAvatar,
+  IonLabel,
+  IonAlert, AlertInput, AlertButton
+} from '@ionic/angular';
 import {GridHomeComponent} from "../../common/grid-home/grid-home.component";
 
 @Component({
   selector: 'app-more',
   templateUrl: './more.page.html',
   styleUrls: ['./more.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, GridHomeComponent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonAvatar, IonChip, IonCol, IonRow, IonGrid, GridHomeComponent, IonAlert]
 })
 export class MorePage implements OnInit {
+  resultado = signal('LOSE');
+  chipSeleccionado: string = '';
+  opciones: AlertInput[] = [
+    {
+      label: 'True',
+      type: 'radio',
+      value: 'true'
+    },
+    {
+      label: 'False',
+      type: 'radio',
+      value: 'false'
+    }
+  ];
+
+  botones: AlertButton[] = [
+    {
+      text: 'CANCEL',
+      role: 'cancel'
+    },
+    {
+      text: 'OK',
+      handler: (valor) => {
+        if (valor !== 'true' && valor !== 'false') {
+          return false; // Mantiene abierto el alert si no se elige
+        }
+
+        //Método que comprueba que chip se ha seleccionado y devuelve win o lose
+        this.comprobarRespuesta(valor);
+        return true;
+      }
+    }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  private comprobarRespuesta(valor: string) {
+    this.resultado.set(
+      valor === this.chipSeleccionado ? 'WIN' : 'LOSE'
+    );
+  }
 }
